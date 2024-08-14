979aa8233e291d125d7be9821433433cd73b5540
igor <igorpasqualino@gmail.com>
2024-08-14 11:51:19 -0300

    Aula 02

--- Appiadas/app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
+++ Appiadas/app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
@@ -1,20 +1,31 @@
 package br.com.fiap.appiadas
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.com.fiap.appiadas.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvJoker)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView( binding.root)
+        binding.btTellJoker.setOnClickListener {
+            showJoker()
+            playSong()
         }
+    }
+    fun showJoker() {
+        val jokers = resources.getStringArray( R.array.jokers)
+        val numberJoker = Random.nextInt( jokers.size)
+        val joker = jokers[numberJoker ]
+        binding.tvJoker.text = joker
+    }
+
+    private  fun playSong(){
+        val mediaPlayer = MediaPlayer.create(this,R.raw.badumtss)
+        mediaPlayer.start()
     }
 }
